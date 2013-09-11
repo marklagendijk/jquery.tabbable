fc417c921316f85590f7d7625fec70279913574f
Mark Lagendijk <mark@lagendijk.info>
2013-09-11 21:32:54 +0200

    Fixed names mixup.

--- jquery.tabbable.js
+++ jquery.tabbable.js
@@ -50,20 +50,6 @@
 	function selectNextTabbableOrFocusable(selector){
 		var selectables = $(selector);
 		var current = $(':focus');
-		var prevIndex = selectables.length - 1;
-		if(current.length === 1){
-			var currentIndex = selectables.index(current);
-			if(currentIndex > 0){
-				prevIndex = currentIndex - 1;
-			}
-		}
-
-		selectables.eq(prevIndex).focus();
-	}
-
-	function selectPrevTabbableOrFocusable(selector){
-		var selectables = $(selector);
-		var current = $(':focus');
 		var nextIndex = 0;
 		if(current.length === 1){
 			var currentIndex = selectables.index(current);
@@ -73,6 +59,20 @@
 		}
 
 		selectables.eq(nextIndex).focus();
+	}
+
+	function selectPrevTabbableOrFocusable(selector){
+		var selectables = $(selector);
+		var current = $(':focus');
+		var prevIndex = selectables.length - 1;
+		if(current.length === 1){
+			var currentIndex = selectables.index(current);
+			if(currentIndex > 0){
+				prevIndex = currentIndex - 1;
+			}
+		}
+
+		selectables.eq(prevIndex).focus();
 	}
 
 	/**
